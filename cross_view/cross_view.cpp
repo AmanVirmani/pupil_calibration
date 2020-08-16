99ec29fe00860407da8b6d03ee486a18e51f06cb
amanvirmani <aman.virmani26@gmail.com>
2020-08-16 19:52:28 -0400

    added visualizer package for rgb-dvs gaze visualization

---
+++ cross_view/cross_view.cpp
@@ -0,0 +1,303 @@
+#include <vector>
+#include <valarray>
+#include <algorithm>
+#include <type_traits>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <mutex>
+
+// #include <X11/Xlib.h>
+
+#include <ros/ros.h>
+#include <ros/package.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+// Local includes
+#include <common.h>
+#include <event.h>
+#include <event_vis.h>
+
+// DVS / DAVIS
+#include <dvs_msgs/Event.h>
+#include <dvs_msgs/EventArray.h>
+
+#include <pupil_ros/gaze_positions.h>
+#include <pupil_ros/gaze.h>
+
+
+
+class Visualizer {
+protected:
+    std::mutex mutex;
+    std::string name, window_name, ctopic, etopic, gaze_topic;
+    ros::Subscriber e_sub, c_sub, gaze_info;
+
+    uint32_t c_res_x, c_res_y, e_res_x, e_res_y;
+    //uint32_t gaze_px, gaze_py, gaze_match_px, gaze_match_py;
+    float gaze_px, gaze_py, gaze_match_px, gaze_match_py;
+    float gaze_Z;
+
+    // Buffer for incoming events (aka 'slice')
+    // 1e8 == 0.1 sec
+    CircularArray<Event, 1000000, 50000000> ev_buffer;
+    cv::Mat c_image;
+
+    cv::Mat c_K, c_D, e_K, e_D;
+    std::string c_model, e_model;
+    //pupil_ros::gaze_positions gaze_point; 
+
+public:
+    float point_x = 600, point_y = 400, point_Z = 500;
+    Visualizer(ros::NodeHandle &nh, std::string ctopic_, std::string etopic_, std::string gaze_topic_ , std::string name_="visualizer")
+        : name(name_), ctopic(ctopic_), etopic(etopic_), gaze_topic(gaze_topic_), c_res_x(0), c_res_y(0), e_res_x(0), e_res_y(0)
+        , gaze_px(100), gaze_py(100), gaze_Z(1.0) {
+        this->window_name = this->name;
+        cv::namedWindow(this->window_name, cv::WINDOW_NORMAL);
+
+        // Set camera intrinsics
+        this->c_K = Visualizer::get_K(801.6947924812483, 802.6485823902558, 620.2055872431376, 354.3864350612314);
+        this->c_D = (cv::Mat1d(1, 4) << -0.13229634176002464, -0.023644289230288832, 0.019704866760515385, -0.0065800357834113);
+        this->c_model = "equidistant";
+
+        this->e_K = Visualizer::get_K(506.4692274538, 504.1712838890729, 325.79676532421803, 238.70837103100686);
+        this->e_D = (cv::Mat1d(1, 4) << 0.13140489691973775, -0.17447930179658452, 0.00044504317597171593, 0.004727272184495526);
+        this->e_model = "radtan";
+
+        //this->gaze_info = nh.subscribe(this->gaze_topic, 10, &Visualizer::callback_gaze, this);
+        this->gaze_info = nh.subscribe(this->gaze_topic, 10, &Visualizer::callback_gaze, this);
+        this->c_sub = nh.subscribe(this->ctopic, 1, &Visualizer::c_cb, this);
+        this->e_sub = nh.subscribe(this->etopic, 1, &Visualizer::e_cb<dvs_msgs::EventArray::ConstPtr>, this);
+
+    }
+
+    std::string get_window_name() {return this->window_name; }
+
+    // Callbacks
+    void c_cb(const sensor_msgs::ImageConstPtr& msg) {
+        const std::lock_guard<std::mutex> lock(this->mutex);
+
+        cv::Mat img;
+        if (msg->encoding == "8UC1") {
+            sensor_msgs::Image simg = *msg;
+            simg.encoding = "mono8";
+            img = cv_bridge::toCvCopy(simg, "bgr8")->image;
+        } else {
+            img = cv_bridge::toCvShare(msg, "bgr8")->image;
+        }
+        this->c_res_y = msg->height;
+        this->c_res_x = msg->width;
+        this->c_image = img.clone();
+    }
+
+    template<class T>
+    void e_cb(const T& msg) {
+        const std::lock_guard<std::mutex> lock(this->mutex);
+        for (uint i = 0; i < msg->events.size(); ++i) {
+            ull time = msg->events[i].ts.toNSec();
+            Event e(msg->events[i].y, msg->events[i].x, time);
+            this->ev_buffer.push_back(e);
+        }
+        this->e_res_y = msg->height;
+        this->e_res_x = msg->width;
+    }
+
+    void set_gaze_debug(float px, float py, float Z) {
+        if ((px >= this->c_res_x) || (py >= this->c_res_y) || (Z <= 0)) return;
+        this->gaze_px = px; this->gaze_py = py; this->gaze_Z = Z;
+
+        cv::Mat T = (cv::Mat1f(1, 3) << -0.019612239964863745, 0.017638802911241643, 0.010431131020442333);
+        cv::Matx33f R(0.9996530617336309, -0.01839391872107208, 0.01885258392549504,
+                      0.01873644874065071, 0.9996595912024482, -0.018156189180298512,
+                      -0.01851220287200333, 0.01850312057584921, 0.9996574077521704);
+
+        cv::Mat R_rodrig;
+        cv::Rodrigues(R, R_rodrig);
+
+        cv::Mat dst;
+        if (this->c_model == "equidistant") {
+            cv::Mat src = cv::Mat(1, 1, CV_32FC2, cv::Scalar(px, py));
+            cv::fisheye::undistortPoints(src, dst, this->c_K, this->c_D);
+        }
+        if (this->c_model == "radtan") {
+            cv::Mat src = cv::Mat(1, 1, CV_32FC2, cv::Scalar(px, py));
+            cv::undistortPoints(src, dst, this->c_K, this->c_D);
+        }
+        auto p = cv::Point3f(dst.at<cv::Vec2f>(0, 0)[0], dst.at<cv::Vec2f>(0, 0)[1], 1) * Z;
+        //this->gaze_px = p.x; this->gaze_py = p.y; this->gaze_Z = p.z;
+
+        
+        float fx = 801.6947924812483,fy = 802.6485823902558, cx = 620.2055872431376, cy = 354.3864350612314;
+                                                                                                    
+        this->gaze_match_px = fx*px + cy; this->gaze_match_py = fy*py + cy;
+        std::cout<<"gaze_pupil: " << this->gaze_px << ", " << this->gaze_py << std::endl;
+        std::cout<<"gaze_dvs: " << this->gaze_match_px << ", " << this->gaze_match_py << std::endl;
+        cv::Mat c_image_ = this->c_image; //.clone();
+        cv::Mat c_undistorted = Visualizer::undistort(c_image_, this->c_K, this->c_D, this->c_model);
+        cv::circle(c_undistorted, cv::Point(this->gaze_px, this->gaze_py), 10, cv::Scalar(0,0,255), -1);
+        cv::circle(c_undistorted, cv::Point(this->gaze_match_px, this->gaze_match_py), 10, cv::Scalar(0,255,0), -1);
+        //cv::circle(c_image_, cv::Point(50, 50), 10, cv::Scalar(0,255,0), -1);
+        //cv::hconcat(e_resized, c_resized, img);
+        cv::imshow(this->window_name, c_undistorted);
+    }
+    
+    
+    void set_gaze(uint32_t px, uint32_t py, float Z) {
+        if ((px >= this->c_res_x) || (py >= this->c_res_y) || (Z <= 0)) return;
+        this->gaze_px = px; this->gaze_py = py; this->gaze_Z = Z;
+
+        cv::Mat T = (cv::Mat1f(1, 3) << -0.019612239964863745, 0.017638802911241643, 0.010431131020442333);
+        cv::Matx33f R(0.9996530617336309, -0.01839391872107208, 0.01885258392549504,
+                      0.01873644874065071, 0.9996595912024482, -0.018156189180298512,
+                      -0.01851220287200333, 0.01850312057584921, 0.9996574077521704);
+
+        cv::Mat R_rodrig;
+        cv::Rodrigues(R, R_rodrig);
+
+        cv::Mat dst;
+        if (this->c_model == "equidistant") {
+            cv::Mat src = cv::Mat(1, 1, CV_32FC2, cv::Scalar(px, py));
+            cv::fisheye::undistortPoints(src, dst, this->c_K, this->c_D);
+        }
+        if (this->c_model == "radtan") {
+            cv::Mat src = cv::Mat(1, 1, CV_32FC2, cv::Scalar(px, py));
+            cv::undistortPoints(src, dst, this->c_K, this->c_D);
+        }
+        auto p = cv::Point3f(dst.at<cv::Vec2f>(0, 0)[0], dst.at<cv::Vec2f>(0, 0)[1], 1) * Z;
+        //this->gaze_px = p.x; this->gaze_py = p.y; this->gaze_Z = p.z;
+        std::vector<cv::Point3f> p_vec = {p};
+
+        cv::Mat pix;
+        if (this->e_model == "equidistant") {
+            cv::fisheye::projectPoints(p_vec, pix, R_rodrig, T, e_K, e_D);
+        }
+        if (this->e_model == "radtan") {
+            cv::projectPoints(p_vec, R_rodrig, T, e_K, e_D, pix);
+        }
+
+        this->gaze_match_px = pix.at<cv::Vec2f>(0, 0)[0]; this->gaze_match_py = pix.at<cv::Vec2f>(0, 0)[1];
+        std::cout<<"gaze_pupil: " << this->gaze_px << ", " << this->gaze_py << std::endl;
+        std::cout<<"gaze_dvs: " << this->gaze_match_px << ", " << this->gaze_match_py << std::endl;
+    }
+
+    void visualize(int e_mode=0) {
+        const std::lock_guard<std::mutex> lock(this->mutex);
+        cv::Mat e_image;
+        if (e_mode % 2 == 0) {
+            e_image = EventFile::color_time_img(&ev_buffer, 1, this->e_res_y, this->e_res_x);
+        } else {
+            e_image = EventFile::projection_img(&ev_buffer, 1, this->e_res_y, this->e_res_x);
+            cv::cvtColor(e_image, e_image, CV_GRAY2RGB);
+        }
+        
+        uint32_t height = std::min(this->c_image.rows, e_image.rows);
+        if ((this->c_image.rows < 1) || (e_image.rows < 1)) return;
+
+        // Draw on source frame
+        cv::Mat c_image_ = this->c_image.clone();
+        cv::circle(c_image_, cv::Point(this->gaze_px, this->gaze_py), 10, cv::Scalar(0,0,255), -1);
+        cv::circle(e_image,  cv::Point(this->gaze_match_px, this->gaze_match_py), 5, cv::Scalar(0,0,255), -1);
+
+        // Resize, undistort
+        cv::Mat c_undistorted = Visualizer::undistort(c_image_, this->c_K, this->c_D, this->c_model);
+        cv::Mat e_undistorted = Visualizer::undistort(e_image,  this->e_K, this->e_D, this->e_model);
+
+		
+	std::cout << "RGB image size: " << c_undistorted.rows << ", " << c_undistorted.cols << std::endl;
+	std::cout << "DVS image size: " << e_undistorted.rows << ", " << e_undistorted.cols << std::endl;
+        //uint32_t e_width = (e_undistorted.cols * height) / e_undistorted.rows;
+        //uint32_t c_width = (c_undistorted.cols * height) / c_undistorted.rows;
+
+        cv::Mat e_resized, c_resized, img;
+        //cv::resize(c_undistorted, c_resized, cv::Size(c_width, height), 0, 0);
+        cv::resize(c_undistorted, c_resized, cv::Size(c_image.cols, c_image.rows), 0, 0);
+        cv::resize(e_undistorted, e_resized, cv::Size(c_image.cols, c_image.rows), 0, 0);
+	std::cout << "RGB image size: " << c_resized.rows << ", " << c_resized.cols << std::endl;
+	std::cout << "DVS image size: " << e_resized.rows << ", " << e_resized.cols << std::endl;
+
+        cv::hconcat(e_resized, c_resized, img);
+        cv::imshow(this->window_name, img);
+    }
+
+    static cv::Mat get_K(float fx, float fy, float cx, float cy) {
+        cv::Mat K = (cv::Mat1d(3, 3) << fx, 0, cx, 0, fy, cy, 0, 0, 1);
+        return K;
+    }
+
+    void callback_gaze(const pupil_ros::gaze_positions &info) {
+        int resx = 1280, resy = 720;
+        //float Z = info.gazes[0].gaze_point_3d.z;//1000; // in m
+        //float X = info.gazes[0].gaze_point_3d.x;//1000;
+        //float Y = info.gazes[0].gaze_point_3d.y;//1000;
+    
+        //cv::Mat worldP=  (cv::Mat1d(3,1) << (X, Y, Z));
+        //cv::Mat dst = this->c_K * worldP;
+        //this->point_Z = dst.at<cv::Vec2f>(0, 0)[2];//dst.at<cv::Vec2f>(0, 0)[2]; //projectedP[2];
+        //this->point_y = dst.at<cv::Vec2f>(0, 0)[1];//dst.at<cv::Vec2f>(0, 0)[2];  //projectedP[1];
+        //this->point_x = dst.at<cv::Vec2f>(0, 0)[0];//dst.at<cv::Vec2f>(0, 0)[2];  //projectedP.at(0,0);
+        this->point_Z = info.gazes[0].gaze_point_3d.z; 
+	if (this->point_Z) {
+		this->point_Z = 0.5;
+	}
+        this->point_x = info.gazes[0].norm_pos.x * resx;
+        this->point_y = (1- info.gazes[0].norm_pos.y) * resy;
+    }
+
+protected:
+    static cv::Mat undistort(cv::Mat &img, cv::Mat K, cv::Mat D, std::string dist_model) {
+        cv::Mat ret;
+        if (dist_model == "radtan") {
+            cv::undistort(img, ret, K, D, 1.0 * K);
+        } else if (dist_model == "equidistant") {
+            cv::fisheye::undistortImage(img, ret, K, D, 1.0 * K);
+        } else {
+            std::cout << "Unknown distortion model! " << dist_model << std::endl;
+        }
+
+        return ret;
+    }
+};
+
+
+void on_trackbar(int, void*) {}
+
+//int point_x = 600, point_y = 400, point_Z = 500;
+
+int main (int argc, char** argv) {
+    std::string node_name = "cross_view";
+    ros::init (argc, argv, node_name);
+    ros::NodeHandle nh("~");
+
+        
+    Visualizer vis(nh, "/pupil_capture/world", "/samsung/camera/events", "/pupil_capture/gaze");
+    //ros::Subscriber gaze_info = nh.subscribe("pupil_capture/gaze", 10, vis.callback_gaze());
+
+    //global int point_x = 600, point_y = 400, point_Z = 500;
+    // cv::createTrackbar("x", vis.get_window_name(), &point_x, 1200, on_trackbar);
+    // cv::createTrackbar("y", vis.get_window_name(), &point_y, 1200, on_trackbar);
+    // cv::createTrackbar("Z", vis.get_window_name(), &point_Z, 1000, on_trackbar);
+
+
+    int e_mode = 0;
+    int code = 0; // Key code
+    while ((code != 27) && ros::ok()) {
+        code = cv::waitKey(20);
+        if (code == 32) e_mode ++;
+
+        //vis.set_gaze_debug(vis.point_x, vis.point_y, float(vis.point_Z + 1) / 100.0);
+        vis.set_gaze(vis.point_x, vis.point_y, float(vis.point_Z + 1) / 100.0);
+        vis.visualize(e_mode);
+
+        ros::spinOnce();
+    }
+
+    ros::shutdown();
+    return 0;
+}
---
+++ cross_view/event_vis.h
@@ -0,0 +1,246 @@
+#include <common.h>
+#include <event.h>
+
+#ifndef EVENT_VIS_H
+#define EVENT_VIS_H
+
+class EventFile {
+public:
+    template<class T> static cv::Mat color_time_img     (T *events, int scale = 0, int res_x = RES_X, int res_y = RES_Y);
+    template<class T> static cv::Mat projection_img     (T *events, int scale = 1, int res_x = RES_X, int res_y = RES_Y);
+    template<class T> static cv::Mat projection_img_unopt (T *events, int scale, int res_x = RES_X, int res_y = RES_Y);
+    static double nonzero_average (cv::Mat img);
+    static void nonzero_norm (cv::Mat img);
+
+    static cv::Vec3b id2rgb(unsigned int id);
+};
+
+
+template<class T> cv::Mat EventFile::projection_img (T *events, int scale, int res_x, int res_y) {
+
+    int scale_img_x = res_x * scale;
+    int scale_img_y = res_y * scale;
+
+    int cnt = 0;
+    cv::Mat best_project_hires_img = cv::Mat::zeros(scale_img_x, scale_img_y, CV_8UC1);
+    for (auto &e : *events) {
+        int x = e.fr_x * scale;
+        int y = e.fr_y * scale;
+
+        if ((x >= scale * (res_x - 1)) || (x < 0) || (y >= scale * (res_y - 1)) || (y < 0))
+            continue;
+
+        x += scale / 2;
+        y += scale / 2;
+
+        cnt ++;
+
+        int lx = std::max(x - scale / 2, 0), ly = std::max(y - scale / 2, 0);
+        int rx = std::min(x + scale / 2, scale_img_x), ry = std::min(y + scale / 2, scale_img_y);
+        for (int jx = lx; jx <= rx; ++jx) {
+            for (int jy = ly; jy <= ry; ++jy) {
+                if (best_project_hires_img.at<uchar>(jx, jy) < 255)
+                    best_project_hires_img.at<uchar>(jx, jy) ++;
+            }
+        }
+    }
+
+    if (scale > 1) {
+        cv::GaussianBlur(best_project_hires_img, best_project_hires_img, cv::Size(scale, scale), 0, 0);
+    }
+
+    double img_scale = 127.0 / EventFile::nonzero_average(best_project_hires_img);
+    cv::convertScaleAbs(best_project_hires_img, best_project_hires_img, img_scale, 0);
+    return best_project_hires_img;
+}
+
+
+template<class T> cv::Mat EventFile::projection_img_unopt (T *events, int scale, int res_x, int res_y) {
+
+    int scale_img_x = res_x * scale;
+    int scale_img_y = res_y * scale;
+
+    int cnt = 0;
+    cv::Mat best_project_hires_img = cv::Mat::zeros(scale_img_x, scale_img_y, CV_8UC1);
+    for (auto &e : *events) {
+        int x = e.fr_x * scale;
+        int y = e.fr_y * scale;
+
+        if ((x >= scale * (res_x - 1)) || (x < 0) || (y >= scale * (res_y - 1)) || (y < 0))
+            continue;
+
+        x += scale / 2;
+        y += scale / 2;
+
+        cnt ++;
+
+        int lx = std::max(x - scale / 2, 0), ly = std::max(y - scale / 2, 0);
+        int rx = std::min(x + scale / 2, scale_img_x), ry = std::min(y + scale / 2, scale_img_y);
+        for (int jx = lx; jx <= rx; ++jx) {
+            for (int jy = ly; jy <= ry; ++jy) {
+                if (best_project_hires_img.at<uchar>(jx, jy) < 255)
+                    best_project_hires_img.at<uchar>(jx, jy) ++;
+            }
+        }
+    }
+
+    if (scale > 1) {
+        cv::GaussianBlur(best_project_hires_img, best_project_hires_img, cv::Size(scale, scale), 0, 0);
+    }
+
+    double img_scale = 127.0 / EventFile::nonzero_average(best_project_hires_img);
+    cv::convertScaleAbs(best_project_hires_img, best_project_hires_img, img_scale, 0);
+    return best_project_hires_img;
+}
+
+
+template<class T> cv::Mat EventFile::color_time_img (T *events, int scale, int res_x, int res_y) {
+    if (scale == 0) scale = 11;
+
+    ull t_min = LLONG_MAX, t_max = 0;
+    uint x_min = res_x, y_min = res_y, x_max = 0, y_max = 0;
+    for (auto &e : *events) {
+        if (e.get_x() > x_max) x_max = e.get_x();
+        if (e.get_y() > y_max) y_max = e.get_y();
+        if (e.get_x() < x_min) x_min = e.get_x();
+        if (e.get_y() < y_min) y_min = e.get_y();
+    }
+
+    for (auto &e : *events) {
+        if (e.timestamp < (sll)t_min) t_min = e.timestamp;
+        if (e.timestamp > (sll)t_max) t_max = e.timestamp;
+    }
+
+    x_max = std::min(x_max, (uint)res_x);
+    y_max = std::min(y_max, (uint)res_y);
+
+    x_min = 0; y_min = 0; x_max = res_x - 1; y_max = res_y - 1;
+
+    if ((x_min > x_max) || (y_min > y_max)) {
+        return cv::Mat(0, 0, CV_8UC3, cv::Scalar(0, 0, 0));
+    }
+
+    int metric_wsizex = scale * (x_max - x_min);
+    int metric_wsizey = scale * (y_max - y_min);
+    int scale_img_x = metric_wsizex + scale;
+    int scale_img_y = metric_wsizey + scale;
+
+    cv::Mat project_img(scale_img_x, scale_img_y, CV_32FC3, cv::Scalar(0, 0, 0));
+    cv::Mat project_img_cnt = cv::Mat::zeros(scale_img_x, scale_img_y, CV_32FC1);
+    cv::Mat project_img_avg(scale_img_x, scale_img_y, CV_8UC3, cv::Scalar(0, 0, 0));
+
+    double x_shift = - double((x_max - x_min) / 2 + x_min) * double(scale) + double(metric_wsizex) / 2.0;
+    double y_shift = - double((y_max - y_min) / 2 + y_min) * double(scale) + double(metric_wsizey) / 2.0;
+    x_shift = 0; y_shift = 0;
+
+
+    int ignored = 0, total = 0;
+    for (auto &e : *events) {
+        total ++;
+        int x = e.fr_x * scale + x_shift;
+        int y = e.fr_y * scale + y_shift;
+
+        if ((x >= metric_wsizex) || (x < 0) || (y >= metric_wsizey) || (y < 0)) {
+            ignored ++;
+            continue;
+        }
+
+        float angle = 2 * 3.14 * (double(e.timestamp - t_min) / double(t_max - t_min));
+
+        if (scale > 1) {
+            x += scale / 2;
+            y += scale / 2;
+        }
+
+        for (int jx = x - scale / 2; jx <= x + scale / 2; ++jx) {
+            for (int jy = y - scale / 2; jy <= y + scale / 2; ++jy) {
+                project_img.at<cv::Vec3f>(jx, jy)[0] += cos(angle);
+                project_img.at<cv::Vec3f>(jx, jy)[1] += sin(angle);
+                project_img_cnt.at<float>(jx, jy) += 1;
+            }
+        }
+    }
+
+    for (int jx = 0; jx < scale_img_x; ++jx) {
+        for (int jy = 0; jy < scale_img_y; ++jy) {
+            if (project_img_cnt.at<float>(jx, jy) < 1) continue;
+
+            float vx = (project_img.at<cv::Vec3f>(jx, jy)[0] / (float)project_img_cnt.at<float>(jx, jy));
+            float vy = (project_img.at<cv::Vec3f>(jx, jy)[1] / (float)project_img_cnt.at<float>(jx, jy));
+
+            double speed = hypot(vx, vy);
+            double angle = 0;
+            if (speed != 0)
+                angle = (atan2(vy, vx) + 3.1416) * 180 / 3.1416;
+
+            project_img_avg.at<cv::Vec3b>(jx, jy)[0] = angle / 2;
+            project_img_avg.at<cv::Vec3b>(jx, jy)[1] = speed * 255;
+            project_img_avg.at<cv::Vec3b>(jx, jy)[2] = 255;
+        }
+    }
+
+    cv::cvtColor(project_img_avg, project_img_avg, CV_HSV2BGR);
+    return project_img_avg;
+}
+
+
+double EventFile::nonzero_average (cv::Mat img) {
+    // Average of nonzero
+    double nz_avg = 0;
+    long int nz_avg_cnt = 0;
+    uchar* p = img.data;
+    for(int i = 0; i < img.rows * img.cols; ++i, p++) {
+        if (*p == 0) continue;
+        nz_avg_cnt ++;
+        nz_avg += *p;
+    }
+    nz_avg = (nz_avg_cnt == 0) ? 0 : nz_avg / double(nz_avg_cnt);
+    return nz_avg;
+}
+
+
+void EventFile::nonzero_norm (cv::Mat img) {
+    double nz_min = 10000, nz_max = 0;
+    long int nz_cnt = 0;
+    uchar* p = img.data;
+    for(int i = 0; i < img.rows * img.cols; ++i, p++) {
+        if (*p <= 0.0001) continue;
+        nz_cnt ++;
+        if (*p > nz_max) nz_max = *p;
+        if (*p < nz_min) nz_min = *p;
+    }
+
+    if (nz_cnt == 0)
+        return;
+
+    p = img.data;
+    for(int i = 0; i < img.rows * img.cols; ++i, p++) {
+        if (*p == 0) continue;
+        *p -= nz_min;
+    }
+
+    double rng = nz_max - nz_min;
+
+    if (rng < 0.0001)
+        return;
+
+    img /= rng;
+}
+
+
+cv::Vec3b EventFile::id2rgb(unsigned int id) {
+    cv::Vec3b ret;
+    int COLORS[6][3] = {{255, 255, 0}, {255, 0, 255}, {0, 255, 255},
+                        {255, 0, 0},   {0, 255, 0},   {0, 0, 255}};
+    if (id == 255)
+        return {255,0,255};
+
+    int len = sizeof(COLORS) / sizeof(*COLORS);
+    ret[0] = COLORS[id % len][0];
+    ret[1] = COLORS[id % len][1];
+    ret[2] = COLORS[id % len][2];
+    return ret;
+}
+
+
+#endif // EVENT_VIS_H
